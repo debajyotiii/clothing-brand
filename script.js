58c66e3b1e67cffe733ae3223dbf6f67df80ee33
Debajyoti Ghosh <ghoshdebajyoti580@gmail.com>
2025-07-13 13:57:34 +0530

    Add files via upload
    Trying for first time

---
+++ script.js
@@ -0,0 +1,6 @@
+function addToCart() {
+  const size = document.getElementById("size").value;
+  const quantity = document.getElementById("quantity").value;
+
+  alert(`Added ${quantity} item(s) of size ${size} to your cart!`);
+}
